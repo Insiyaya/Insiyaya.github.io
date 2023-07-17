7a4b62294cf8a7d704ab96837aa6bab3d1b8a9cf
insiya <insiyahusain3@gmail.com>
2023-07-17 19:47:48 +0530

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+
+  var firebaseConfig = {
+    apiKey : "AIzaSyAmtwzy1Vl86kL6O-mOxXGBmzvjXe-sils",
+    authDomain: "naman-jain-p1.firebaseapp.com",
+    projectId: "naman-jain-p1",
+    // ... (other credentials)
+  };
+  firebase.initializeApp(firebaseConfig);
